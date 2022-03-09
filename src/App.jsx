1d75201679c1a71bb62d35ec23a2591e43abf5f8
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2022-03-09 14:00:30 +0100

    Timer & EditTimer

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,21 @@
 import { useState } from 'react'
-import logo from './logo.svg'
+import EditTimer from './components/EditTimer'
+import Timer from './components/Timer'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isEditable, setIsEditable] = useState(false)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="container">
+      <h1>Hello World</h1>
+      <div className="outerRing">
+        <div className="timer">
+          <div id="id">
+            <EditTimer />
+            <Timer />
+          </div>  
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/EditTimer.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const EditTimer = () => {
+    return (
+        <div>
+            <h1>Edit Timer</h1>
+        </div>
+    );
+};
+
+export default EditTimer;
